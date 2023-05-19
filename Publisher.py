d1a56a664c0919cd125c3d22597505cbfe6b5539
Riya791 <92538522+Riya791@users.noreply.github.com>
2023-05-19 18:07:49 +0530

    Add files via upload

---
+++ Publisher.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+broker_address = "localhost"
+broker_port = 1883
+topic = "Task"
+
+# Create a MQTT client
+client = mqtt.Client()
+
+# Connect to the MQTT broker
+client.connect(broker_address, broker_port)
+
+# Publish a message to the topic
+message = "Hello, I am Ria!"
+client.publish(topic, message)
